e892c2d26ba06110bd45a65a6adac73f053cda47
Dmitry Klionsky <dm.klionsky@gmail.com>
2020-10-31 22:48:10 +0300

    Add example

---
+++ ch4-Containers/Showing_And_Hiding_Widgets.js
@@ -0,0 +1,45 @@
+const gi = require('node-gtk');
+const Gtk = gi.require('Gtk', '3.0');
+
+gi.startLoop();
+Gtk.init();
+
+class AppWindow extends Gtk.ApplicationWindow {
+    constructor(args) {
+        super(args);
+        this.on('show', Gtk.main);
+        this.on('destroy', Gtk.mainQuit);
+
+        const expander = new Gtk.Expander.newWithMnemonic('Click _Me For More!');
+        const label = new Gtk.Label({label: 'Hide me or show me,\nthat is your choice.'});
+        expander.add(label);
+        expander.setExpanded(true);
+        this.add(expander);
+
+        this.setBorderWidth(10);
+        this.setSizeRequest(200, 100);
+    }
+}
+
+class App extends Gtk.Application {
+    constructor(args) {
+        super({
+            application_id: 'org.example.myapp',
+            ...args
+        });
+        this.window = null;
+        this.on('activate', this.onActivate);
+    }
+
+    onActivate() {
+        if (!this.window) {
+            this.window = new AppWindow({
+                title: 'Show & Hide',
+            });
+            this.window.showAll();
+        }
+    }
+}
+
+const app = new App();
+app.run();
